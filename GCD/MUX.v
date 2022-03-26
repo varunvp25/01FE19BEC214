e44c3ce4299bd929dd2145e517a1b4f848db6c4a
varunvp25 <59868836+varunvp25@users.noreply.github.com>
2022-03-26 21:49:08 +0530

    Update MUX.v

--- GCD/MUX.v
+++ GCD/MUX.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:47:10 02/14/2022 
-// Design Name: 
-// Module Name:    MUX 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
  module MUX(out,in0,in1,sel);
 input [15:0] in0,in1;
 input sel;
