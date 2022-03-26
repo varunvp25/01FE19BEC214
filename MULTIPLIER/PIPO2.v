07a2b25f2d4203b1f62f66511c6a222f80c0dd52
varunvp25 <59868836+varunvp25@users.noreply.github.com>
2022-03-26 21:57:35 +0530

    Update PIPO2.v

--- MULTIPLIER/PIPO2.v
+++ MULTIPLIER/PIPO2.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:26:56 02/17/2022 
-// Design Name: 
-// Module Name:    PIPO2 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module PIPO2(dout,din,ld,clr,clk);
 input [15:0]din;
 input ld,clr,clk;
