b33ef96ccb6d7af8506049b3896f8fe73d22081b
varunvp25 <59868836+varunvp25@users.noreply.github.com>
2022-03-26 21:48:24 +0530

    Update COMPARE.v

--- GCD/COMPARE.v
+++ GCD/COMPARE.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:54:59 02/14/2022 
-// Design Name: 
-// Module Name:    COMPARE 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module COMPARE(lt,gt,eq,data1,data2);
 
 input [15:0] data1,data2 ;
